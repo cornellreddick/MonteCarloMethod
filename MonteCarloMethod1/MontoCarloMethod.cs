a1c707204ad1df05bf382ed811acb29fd8f2e279
conrellreddick <cornellreddick@outlook.com>
2018-03-25 00:50:15 -0400

    First commit for MonteCarlo

---
+++ MonteCarloMethod1/MontoCarloMethod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonteCarloMethod1
+{
+    struct Coordinates
+    {
+        private double x, y;
+
+        public double X { get => x; set => x = value; }
+        public double Y { get => y; set => y = value; }
+
+        public Coordinates(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Coordinates(Random Object)
+        {
+            this.x = Object.NextDouble();
+            this.y = Object.NextDouble();
+
+        }
+        
+        public double hypot(double x, double y)
+        {
+            return Math.Sqrt((x * x) + (y * y));
+        }
+    }
+
+}
---
+++ MonteCarloMethod1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonteCarloMethod1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int inCircle = 0;
+           
+            double probability = 0;
+            Random rand = new Random();
+
+            Console.WriteLine("Enter a random number:");
+            int length = int.Parse(Console.ReadLine());
+
+            Coordinates[] arr = new Coordinates[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                arr[i] = new Coordinates(rand);
+                double h = arr[i].hypot(arr[i].X, arr[i].Y);
+                //Console.WriteLine($" h: {h}");
+                if (h > 1)
+                {
+                    
+                }
+                else if (h <= 1 && h >= 0)
+                {
+                    counter++;
+                }
+                else
+                {
+                    Console.WriteLine("invalid hyptonuse");
+                }
+
+                //Console.WriteLine(counter);
+            }
+            Console.WriteLine(counter);
+
+            inCircle = counter;
+
+            probability = 4.0 * ((double)inCircle / (double)length);
+
+            Console.WriteLine();
+            Console.WriteLine($"Approximated Pi = {probability.ToString()}, and actual PI = {Math.PI}");
+            double difference = Math.Abs(Math.PI - probability);
+            Console.WriteLine($" The Absolute value is: {difference}");
+            Console.ReadLine();
+        }
+        
+    }
+}
